a0a777d65538c0399ce04eb2498753567621f435
JudgeDev <judge.py@gmail.com>
2020-09-18 15:34:39 +0200

    First part of FT specced out in comments, and uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,32 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        """Runs before each test"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        """Runs after each test"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Test method run by test runner"""
+        # Z has just got home from work and needs to cook something fast before his
+        # wife gets back from an all day shopping trip at PEP.
+        # He desperately searches the internet and sees an interesting site called
+        # "askfridge.com"
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header mention a fridge
+        self.assertIn('askfridge', self.browser.title)
+        self.fail('Finish the test...')
+
+        # He is invited to enter ...
+
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        unittest.main(warnings='ignore')
