e6bead38efb19e31a6bac880f9fa1784036e4f97
JudgeDev <judge.py@gmail.com>
2020-11-20 17:59:49 +0100

    typo in SITENAME

--- askfridge/settings.py
+++ askfridge/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE' in os.environ:  # test flag for debug mode off
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITE_NAME']]
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:  # fall back to insecure settings
     DEBUG = True
     SECRET_KEY = 'insecure-key-for-dev'
